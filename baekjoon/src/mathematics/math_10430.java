86dd762b83ddc8193049dbd5676a0f69297a3dbd
psj98 <park98sj1@hanmail.net>
2023-01-29 02:59:16 +0900

    코딩 테스트 - 기초 - 수학

---
+++ baekjoon/src/mathematics/math_10430.java
@@ -0,0 +1,19 @@
+package mathematics;
+
+import java.util.*;
+
+public class math_10430 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+
+        scan.close();
+    }
+}
