1f7c62f33e9d1363524af80b74bcebbb0c1e12d7
psj98 <park98sj1@hanmail.net>
2023-02-01 20:50:37 +0900

    java bruteforce recursion

--- baekjoon/src/bruteforce/recursion/recursion_9095.java
+++ baekjoon/src/bruteforce/recursion/recursion_9095.java
@@ -1,11 +1,11 @@
-package bruteforce.basic;
+package bruteforce.recursion;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class basic_9095 {
+public class recursion_9095 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
