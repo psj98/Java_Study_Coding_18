d78d44a16c2e7ae10da20d1f917efd8523ce4a83
psj98 <60167488+psj98@users.noreply.github.com>
2023-02-08 11:48:38 +0900

    23.02.01 문제 풀기 정리
    23.02.01 문제 풀기 정리

--- study/src/study_230201/problemset/boj_1021.java
+++ study/src/study_230201/problemset/boj_1021.java
@@ -11,12 +11,13 @@ public class boj_1021 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Deque<Integer> dq = new ArrayDeque<>();
-        int cnt = 0;
+        int cnt = 0; // 횟수
 
         String[] str = br.readLine().split(" ");
         int n = Integer.parseInt(str[0]);
         int m = Integer.parseInt(str[1]);
 
+        // 덱에 값 순서대로 저장
         for (int i = 1; i <= n; i++)
             dq.add(i);
 
@@ -26,6 +27,7 @@ public class boj_1021 {
             int right = 0, left = 0;
             Iterator<Integer> it = dq.iterator();
 
+            // 입력 받은 수를 뽑으려면 몇 번을 옮겨야하는지 찾기 (왼쪽에서 몇 번, 오른쪽에서 몇 번)
             while (it.hasNext()) {
                 int num = it.next();
                 if (num == Integer.parseInt(str[i]))
@@ -35,6 +37,7 @@ public class boj_1021 {
 
             right = dq.size() - left;
 
+            // 원하는 수가 나올 때까지 옮기고 뽑기
             if (left <= right) {
                 for (int j = 0; j < left; j++) {
                     dq.addLast(dq.getFirst());
