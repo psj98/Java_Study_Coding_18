c414fccdda6c6a912ff8ab3f7708fc251446a027
psj98 <60167488+psj98@users.noreply.github.com>
2023-02-08 12:07:56 +0900

    23.02.01 문제 풀기 정리
    23.02.01 문제 풀기 정리

--- study/src/study_230201/problemset/boj_19583_2.java
+++ study/src/study_230201/problemset/boj_19583_2.java
@@ -11,7 +11,7 @@ public class boj_19583_2 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<String, Integer> mp = new HashMap<>();
+        Map<String, Integer> mp = new HashMap<>(); // 이름, 시간(분)
         int[] time = new int[3]; // S, E, Q
         int ans = 0;
 
@@ -20,20 +20,20 @@ public class boj_19583_2 {
             String str = stk.nextToken();
             int h = Integer.parseInt(str.substring(0, 2)) * 60;
             int m = Integer.parseInt(str.substring(3));
-            time[i] = h + m;
+            time[i] = h + m; // 분으로 계산해서 저장
         }
 
         String line;
         while ((line = br.readLine()) != null) {
             stk = new StringTokenizer(line);
             String str = stk.nextToken();
-            int chat = Integer.parseInt(str.substring(0, 2)) * 60 + Integer.parseInt(str.substring(3));
+            int chat = Integer.parseInt(str.substring(0, 2)) * 60 + Integer.parseInt(str.substring(3)); // 시간 계산
             String name = stk.nextToken();
 
-            if (chat <= time[0])
+            if (chat <= time[0]) // 입장 확인
                 mp.put(name, chat);
-            else if (chat >= time[1] && chat <= time[2] && mp.containsKey(name)) {
-                mp.remove(name);
+            else if (chat >= time[1] && chat <= time[2] && mp.containsKey(name)) { // 퇴장 확인
+                mp.remove(name); // 확인된 사람 제거
                 ans++;
             }
         }
