6ff1032798ee87a6fa70a67bd00a57cf8fb93b33
psj98 <60167488+psj98@users.noreply.github.com>
2023-02-08 12:05:44 +0900

    23.02.01 문제 풀기 정리
    23.02.01 문제 풀기 정리

--- study/src/study_230201/problemset/boj_19583.java
+++ study/src/study_230201/problemset/boj_19583.java
@@ -8,8 +8,8 @@ import java.util.Set;
 public class boj_19583 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<String> in = new HashSet<>();
-        Set<String> out = new HashSet<>();
+        Set<String> in = new HashSet<>(); // 들어온 사람 이름
+        Set<String> out = new HashSet<>(); // 나간 사람 이름
 
         String line = null;
         int ans = 0;
@@ -19,12 +19,13 @@ public class boj_19583 {
         while ((line = br.readLine()) != null) {
             String[] str = line.split(" "); // time, name
 
-            if (str[0].compareTo(time[0]) <= 0)
+            if (str[0].compareTo(time[0]) <= 0) // 입장 확인
                 in.add(str[1]);
-            else if (str[0].compareTo(time[1]) >= 0 && str[0].compareTo(time[2]) <= 0)
+            else if (str[0].compareTo(time[1]) >= 0 && str[0].compareTo(time[2]) <= 0) // 퇴장 확인
                 out.add(str[1]);
         }
 
+        // 입장한 사람이 퇴장한 Set에 있으면 증가
         for (String s : in) {
             if (out.contains(s))
                 ans++;
