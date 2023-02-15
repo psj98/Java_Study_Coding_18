c53490fcfe113946e4ba7bb8a580e3d597e39175
psj98 <60167488+psj98@users.noreply.github.com>
2023-02-15 11:25:10 +0900

    23.02.08 문제 풀기 정리
    23.02.08 문제 풀기 정리

--- study/src/study_230208/problemset/boj_2910_2.java
+++ study/src/study_230208/problemset/boj_2910_2.java
@@ -26,24 +26,31 @@ public class boj_2910_2 {
 		stk = new StringTokenizer(br.readLine());
 		while (stk.hasMoreTokens()) {
 			String num = stk.nextToken();
+
+			// map에 없는 수
 			if (!map.containsKey(num))
 				map.put(num, new int[] { idx++, 1 });
-			else
+			else // map에 있는 수
 				map.put(num, new int[] { map.get(num)[0], map.get(num)[1] + 1 }); // 위치 그대로, 횟수 증가
 		}
 
+		// 정렬을 위해 List<Entry<>>로 변경
 		List<Entry<String, int[]>> list = new ArrayList<>(map.entrySet());
 
 		// 정렬
 		Collections.sort(list, new Comparator<Entry<String, int[]>>() {
 			@Override
 			public int compare(Entry<String, int[]> o1, Entry<String, int[]> o2) {
+				// 횟수가 같으면 위치가 앞인 값을 먼저 출력
 				if (o1.getValue()[1] == o2.getValue()[1])
 					return o1.getValue()[0] < o2.getValue()[0] ? -1 : 1;
+
+				// 횟수가 다르면 횟수가 많은 값을 먼저 출력
 				return o1.getValue()[1] > o2.getValue()[1] ? -1 : 1;
 			}
 		});
-		
+
+		// 횟수만큼 출력
 		for (Entry<String, int[]> entry : list)
 			for (int j = 0; j < entry.getValue()[1]; j++)
 				sb.append(entry.getKey()).append(" ");
