34f45c11309ade01590cf80e31397b8c86430ad4
psj98 <60167488+psj98@users.noreply.github.com>
2023-02-15 11:44:47 +0900

    23.02.08 문제 풀기 정리
    23.02.08 문제 풀기 정리

--- study/src/study_230208/problemset/boj_14627.java
+++ study/src/study_230208/problemset/boj_14627.java
@@ -11,24 +11,26 @@ public class boj_14627 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stk = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int start = 1, end = 1000000000;
-		long paSum = 0;
+
+		int start = 1, end = 1000000000; // 파 최소 길이, 최대 길이
+		long paSum = 0; // 파 길이 합
 
 		int s = Integer.parseInt(stk.nextToken());
 		int c = Integer.parseInt(stk.nextToken());
-		int[] pa = new int[c];
+		int[] pa = new int[s]; // 파 길이 배열
 
 		for (int i = 0; i < s; i++) {
 			pa[i] = Integer.parseInt(br.readLine());
-			paSum += pa[i];
+			paSum += pa[i]; // 각 파의 길이를 더함
 		}
 
+		// 이분 탐색
 		while (start <= end) {
 			int mid = (start + end) / 2;
 			int cnt = 0;
 
 			for (int i = 0; i < s; i++) {
-				cnt += pa[i] / mid; // �� �� ���� ���� ����
+				cnt += pa[i] / mid; // 각 파 별로 개수 세기
 			}
 
 			if (cnt >= c) {
@@ -38,6 +40,7 @@ public class boj_14627 {
 			}
 		}
 
+		// 타입 변환 및 출력
 		sb.append(paSum - end * (long) c);
 		System.out.println(sb);
 	}
