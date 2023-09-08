145103f3cbb2cdb0cbbe0c219b88d9421e474424
psj98 <park98sj1@hanmail.net>
2023-09-09 01:45:52 +0900

    Feat : 23.09.06 문제 풀기 정리

---
+++ study/src/study_230906/problemset/boj_1106.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class boj_1106 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        /**
+         * 적어도 C명 늘리기 위해 투자해야 할 돈의 최솟값
+         * => 얻을 수 있는 고객의 수는 100 이하
+         * => new int[c + 101];
+         */
+        int c = Integer.parseInt(stk.nextToken());
+        int n = Integer.parseInt(stk.nextToken());
+
+        // 배열 초기화
+        int[] dp = new int[c + 101];
+        Arrays.fill(dp, 987654321);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            stk = new StringTokenizer(br.readLine());
+            int cost = Integer.parseInt(stk.nextToken());
+            int customer = Integer.parseInt(stk.nextToken());
+
+            // 이전 값부터 최솟값 갱신
+            for (int j = customer; j < c + 101; j++) {
+                dp[j] = Math.min(dp[j], cost + dp[j - customer]);
+            }
+        }
+
+        // 최솟값 구하기
+        int ans = Integer.MAX_VALUE;
+        for (int i = c; i < c + 101; i++) {
+            ans = Math.min(ans, dp[i]);
+        }
+
+        // 정답 출력
+        sb.append(ans);
+        System.out.println(sb);
+    }
+}
