1e8d710ff75454cecb94256ae4c0839efc4b6bc4
psj98 <park98sj1@hanmail.net>
2023-10-14 23:24:27 +0900

    Feat : 23.10.11 문제 풀기 정리

---
+++ study/src/study_231011/problemset/boj_5972.java
@@ -0,0 +1,17 @@
+package study_231011.problemset;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_5972 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        System.out.println(sb);
+    }
+}
