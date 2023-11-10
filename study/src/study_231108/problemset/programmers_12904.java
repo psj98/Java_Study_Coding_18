0a6f206eaaf220d0ad742add4ca545e02f19252c
psj98 <park98sj1@hanmail.net>
2023-11-11 00:46:49 +0900

    Feat : 23.11.08 문제 풀기 정리

---
+++ study/src/study_231108/problemset/programmers_12904.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(String s) {
+        // 길이가 긴 것부터 체크
+        for (int i = s.length(); i > 0; i--) {
+            for (int j = 0; j + i <= s.length(); j++) {
+                // 팰린드롬 체크
+                if (check(s, j, j + i - 1)) {
+                    return i;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    // 팰린드롬 체크
+    boolean check(String s, int start, int end) {
+        // 양쪽에서부터 가운데로 오면서 체크
+        while (start <= end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
